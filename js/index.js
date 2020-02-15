0dc95fdde281178b566016210eaa33d35a1e04e7
azrael3069 <cmrivera0004@student.stcc.edu>
2020-02-14 20:05:16 -0500

    3 events added so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+// PRESS A KEYBOARD KEY AND THE HEADER WILL DISAPPEAR FROM VISION;
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
+body.addEventListener("keydown", () => (header.style.opacity = "0"));
+
+const navContainer = document.querySelector(".nav-container");
+
+// ADDS AN ALERT WHEN THE USER FIRST SCROLLS WITH THE MOUSE WHEEL BUT THEM REMOVES THAT EVENT SO THEY CAN ACTUALLY NAVIGATE THE SITE
+body.addEventListener("wheel", function alertMeOnce() {
+  alert("WEEEEEEEEEEEE!!!!!!");
+  body.removeEventListener("wheel", alertMeOnce);
+});
+
+const funBusBanner = document.querySelector(".intro > img");
+
+// DRAG THE FUN-BUS INTRO IMAGE TO SCALE IT 150% IN SIZE
+funBusBanner.addEventListener(
+  "drag",
+  e => (e.target.style.transform = "scale(1.5)")
+);
